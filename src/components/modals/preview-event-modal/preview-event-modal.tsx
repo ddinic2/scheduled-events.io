ad6c46b325c9f71d9e104f15d0c936f39b836b4d
dnd_constuctive <dnd@constructive.io>
2025-02-12 13:09:29 +0100

    longer remark

--- src/components/modals/preview-event-modal/preview-event-modal.tsx
+++ src/components/modals/preview-event-modal/preview-event-modal.tsx
@@ -13,7 +13,7 @@ const data = [
     { title: 'Subject', description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry`s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.' },
     {
       title: 'Remarks',
-      description: 'This is details of activity',
+      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry`s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ',
     },
   ];
 
